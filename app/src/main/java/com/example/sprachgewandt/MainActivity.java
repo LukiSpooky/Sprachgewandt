b4ab958789c9ee380a49346d49e2ab351f716d52
Luke Huser <luke.huser01@gmail.com>
2020-07-03 13:06:47 +0200

    animations done

--- app/src/main/java/com/example/sprachgewandt/MainActivity.java
+++ app/src/main/java/com/example/sprachgewandt/MainActivity.java
@@ -2,18 +2,22 @@ package com.example.sprachgewandt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView titel = null;
-    Button synonym = null;
-    Button sprache = null;
-    Button optionen = null;
+    TextView titel;
+    Button synonym;
+    Button sprache;
+    Button optionen;
     Intent synIntent;
     Intent sprIntent;
     Intent optIntent;
@@ -23,15 +27,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button synonym = (Button) findViewById(R.id.synonym);
-        Button sprache = (Button) findViewById(R.id.sprache);
-        Button optionen = (Button) findViewById(R.id.optionen);
+        final Button synonym = (Button) findViewById(R.id.synonym);
+        final Button sprache = (Button) findViewById(R.id.sprache);
+        final Button optionen = (Button) findViewById(R.id.optionen);
+
+
 
         synonym.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+            Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.zoomin);synonym.startAnimation(animation);
             synIntent = new Intent(MainActivity.this, SynonymActivity.class);
-
             startActivity(synIntent);
             }
         });
@@ -39,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         sprache.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.zoomin);
+                sprache.startAnimation(animation);
             sprIntent = new Intent(MainActivity.this, SpracheActivity.class);
             startActivity(sprIntent);
             }
@@ -47,12 +55,16 @@ public class MainActivity extends AppCompatActivity {
         optionen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            optIntent = new Intent(MainActivity.this, OptionenActivity.class);
-            startActivity(optIntent);
+                Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.zoomin);
+                optionen.startAnimation(animation);
+                optIntent = new Intent(MainActivity.this, OptionenActivity.class);
+                startActivity(optIntent);
             }
         });
 
 
 
     }
+
+
 }
